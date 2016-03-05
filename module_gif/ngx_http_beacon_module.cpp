49636e3e1ac82e1aee5dfc4feb52e8f9d6b7312d
yuokada <callistoiv+git@gmail.com>
2016-03-05 20:08:59 +0900

    tmp

--- module_gif/ngx_http_beacon_module.cpp
+++ module_gif/ngx_http_beacon_module.cpp
@@ -113,9 +113,12 @@ ngx_http_empty_gif_handler(ngx_http_request_t *r)
 {
   ngx_http_complex_value_t cv;
 
-  if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD))) {
+  if (!(r->method & (NGX_HTTP_POST | NGX_HTTP_HEAD))) {
     return NGX_HTTP_NOT_ALLOWED;
   }
+
+  // debug: Logging POST BODY
+  ngx_log_error(NGX_LOG_INFO, r->connection->log, 0, "client debug code");
 
   ngx_memzero(&cv, sizeof(ngx_http_complex_value_t));
 
@@ -132,7 +135,7 @@ ngx_http_empty_gif(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
   ngx_http_core_loc_conf_t *clcf;
 
-  clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+  clcf = reinterpret_cast<ngx_http_core_loc_conf_t *>(ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module));
   clcf->handler = ngx_http_empty_gif_handler;
 
   return NGX_CONF_OK;
